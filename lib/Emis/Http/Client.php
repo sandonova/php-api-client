78ee6c05c8270b86018d996d573af844110d9c6e
TGatev <tgatev@emis.com>
2016-12-02 17:30:01 +0200

    Switch dev url to work wit sgorecki host

--- lib/Emis/Http/Client.php
+++ lib/Emis/Http/Client.php
@@ -6,7 +6,8 @@ use Emis\Entity\Serializer;
 class Client
 {
     const CLIENT_TIMEOUT = 120;
-    const API_URL = 'https://api.emis.com/%s/%s/%s/?%s';
+    //const API_URL = 'https://api.emis.com/%s/%s/%s/?%s';
+    const API_URL = 'https://sgorecki-emis-dev.emis.com/php/api/%s/?class=%s&method=%s&%s';
 	
 	const DOCUMENTS_API = 'news';
 	const COMPANY_API = 'company';
