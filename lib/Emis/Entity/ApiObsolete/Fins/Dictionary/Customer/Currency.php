a6841f9d56d7279bf5b1909a276f4cf8ad1ed8ff
Ivan Zhekov <izhekov@emis.com>
2017-01-17 09:56:15 +0200

    Removed obsolete classes.

--- lib/Emis/Entity/ApiObsolete/Fins/Dictionary/Customer/Currency.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Emis\Entity\Api\Fins\Dictionary\Employee;
-/**
- * @author tgatev
- *
- */
-class Currency extends \Emis\Entity\Api\Fins\Dictionary\Currency{
-	
-}
--- lib/Emis/Entity/ApiObsolete/Fins/Dictionary/Customer/GlobalLegalForm.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Emis\Entity\Api\Fins\Dictionary\Customer;
-/**
- * @author tgatev
- *
- */
-class GlobalLegalForm extends \Emis\Entity\Api\Fins\Dictionary\GlobalLegalForm{
-	
-}
